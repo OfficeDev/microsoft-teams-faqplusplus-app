e791907d3a60a2af505318e5d43161785d0ad3c6
Adrian Solis <aosolis@microsoft.com>
2019-07-24 18:29:29 -0700

    Make the QnAMaker implementation a singleton. We don't expect the KB ID to change frequently over the lifetime of the app, so in this case we are caching the QnAMaker instance corresponding to the KB ID.

---
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Services/QnAMakerFactory.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="QnAMakerFactory.cs" company="PlaceholderCompany">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.FAQPlusPlus.Services
+{
+    using System.Collections.Concurrent;
+    using System.Net.Http;
+    using Microsoft.Bot.Builder.AI.QnA;
+    using Microsoft.Bot.Configuration;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Produces the right <see cref="QnAMaker"/> instance for a knowledge base.
+    /// </summary>
+    public class QnAMakerFactory : IQnAMakerFactory
+    {
+        private readonly IConfiguration configuration;
+        private readonly HttpClient httpClient;
+        private readonly ConcurrentDictionary<string, QnAMaker> qnaMakerInstances;
+
+        public QnAMakerFactory(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.httpClient = new HttpClient();
+            this.qnaMakerInstances = new ConcurrentDictionary<string, QnAMaker>();
+        }
+
+        /// <inheritdoc/>
+        public QnAMaker GetQnAMaker(string knowledgeBaseId)
+        {
+            return this.qnaMakerInstances.GetOrAdd(knowledgeBaseId, (kbId) =>
+                new QnAMaker(
+                    new QnAMakerService
+                    {
+                        KbId = kbId,
+                        EndpointKey = this.configuration["EndpointKey"],
+                        Hostname = this.configuration["KbHost"],
+                        SubscriptionKey = this.configuration["QnAMakerSubscriptionKey"],
+                    },
+                    null,
+                    this.httpClient));
+        }
+    }
+}
