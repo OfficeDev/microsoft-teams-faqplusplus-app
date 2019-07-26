7c74f4f5e3401a5e0a6b67f093e3b081049d3422
Adrian Solis <aosolis@microsoft.com>
2019-07-26 11:18:04 -0700

    Enable stylecop config for FaqPlusPlus project.

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Services/IQnAMakerFactory.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Services/IQnAMakerFactory.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IQnAMakerFactory.cs" company="PlaceholderCompany">
+﻿// <copyright file="IQnAMakerFactory.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
