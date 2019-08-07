60e3a29ad5fffb686d586860fd3734ba29b86599
Adrian Solis <aosolis@microsoft.com>
2019-08-06 18:30:32 -0700

    Change SITE_ROLE to configuration instead of config.

--- deploy.cmd
+++ deploy.cmd
@@ -3,10 +3,10 @@
 IF "%SITE_ROLE%" == "bot" (
   deploy.bot.cmd
 ) ELSE (
-  IF "%SITE_ROLE%" == "config" (
-    deploy.config.cmd
+  IF "%SITE_ROLE%" == "configuration" (
+    deploy.configuration.cmd
   ) ELSE (
-    echo You have to set SITE_ROLE setting to either "bot" or "config"
+    echo You have to set SITE_ROLE setting to either "bot" or "configuration"
     exit /b 1
   )
 )
