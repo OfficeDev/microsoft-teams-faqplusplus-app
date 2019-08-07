22e3cb387a9c6aa6b9c98816b69e2300685952b2
Adrian Solis <aosolis@microsoft.com>
2019-08-06 18:17:50 -0700

    Update SITE_KIND to SITE_ROLE Update azuredeploy.json and deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,12 +1,12 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
-IF "%SITE_KIND%" == "bot" (
+IF "%SITE_ROLE%" == "bot" (
   deploy.bot.cmd
 ) ELSE (
-  IF "%SITE_KIND%" == "config" (
+  IF "%SITE_ROLE%" == "config" (
     deploy.config.cmd
   ) ELSE (
-    echo You have to set SITE_KIND setting to either "bot" or "config"
+    echo You have to set SITE_ROLE setting to either "bot" or "config"
     exit /b 1
   )
 )
