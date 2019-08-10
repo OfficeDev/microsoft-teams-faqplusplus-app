4d9193a827e6f6140f9db47a17633d14182a352d
anuragysingh <anurag.y.singh@accenture.com>
2019-08-10 12:15:53 +0530

    Removed check for N/A in Message Extension adaptive card

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/MessagingExtensionTicketsCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/MessagingExtensionTicketsCard.cs
@@ -89,10 +89,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
             if (ticketModel.DateClosed != null)
             {
                 string closedDate = CardHelper.GetTicketClosedDate(this.ticketModel, localTimestamp);
-                if (!closedDate.Equals(Resource.NonApplicableString))
-                {
-                    adaptivefacts.Add(new AdaptiveFact { Title = Resource.ClosedFactTitle, Value = closedDate });
-                }
+                adaptivefacts.Add(new AdaptiveFact { Title = Resource.ClosedFactTitle, Value = closedDate });
             }
 
             return adaptivefacts;
